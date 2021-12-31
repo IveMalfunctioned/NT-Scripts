effe3fb0f6f0c27400f29d0873acb8c916b54975
IveMalfunctioned <20033421+IveMalfunctioned@users.noreply.github.com>
2021-12-30 22:07:31 -0500

    Add files via upload

--- Restore Corrupted System Files.bat
+++ Restore Corrupted System Files.bat
@@ -4,12 +4,9 @@
     if %errorLevel% == 0 (
 	dism /online /cleanup-image /restorehealth
 	sfc /scannow
-        set /p answer=Cleanup complete, restart? (make sure you save your work first) (y/n)
-	if "%answer%"=="y" (GOTO restart)
-	if "%answer%"=="n" (exit /b)
-	:restart
-		shutdown /r /t 0
-	:norestart
+        set /p answer=Cleanup complete press any key to restart (make sure you save your work first)
+	PAUSE
+	shutdown /r /t 0
 	exit
     ) else (
         echo Administrator permissions required to run this script
