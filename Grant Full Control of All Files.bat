cf5a51bac296fedb48b6715508306c66908e785d
IveMalfunctioned <20033421+IveMalfunctioned@users.noreply.github.com>
2022-04-01 23:00:07 -0400

    Add files via upload

---
+++ Grant Full Control of All Files.bat
@@ -0,0 +1,10 @@
+::Run this with terminal-SYS, use Command Prompt with SYSTEM permissions.
+::Add more files or directories if needed
+::NOTE - This will take a while to run, so you'll probably want to run it overnight
+takeown /f "C:\Program Files" /a /r /d Y
+takeown /f "C:\Program Files (x86)" /a /r /d Y
+takeown /f "C:\ProgramData" /a /r /d Y
+takeown /f "C:\Users" /a /r /d Y
+takeown /f "C:\Windows" /a /r /d Y
+icacls C:\ /grant Administrators:F /T /C
+PAUSE
