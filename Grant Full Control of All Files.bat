26ed8cf3a4a30bad76ada6253a22cde161b013d4
IveMalfunctioned <20033421+IveMalfunctioned@users.noreply.github.com>
2022-04-02 00:20:15 -0400

    Update Grant Full Control of All Files.bat

--- Grant Full Control of All Files.bat
+++ Grant Full Control of All Files.bat
@@ -1,6 +1,6 @@
 ::Run this with terminal-SYS, use Command Prompt with SYSTEM permissions.
 ::Add more files or directories if needed
-::NOTE - This will take a while to run, so you'll probably want to run it overnight
+::NOTE - This will take a while to run, so you'll probably want to run it overnight. Run it with as little programs running as possible to prevent conflicts.
 takeown /f "C:\Program Files" /a /r /d Y
 takeown /f "C:\Program Files (x86)" /a /r /d Y
 takeown /f "C:\ProgramData" /a /r /d Y
